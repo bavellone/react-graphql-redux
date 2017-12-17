5b0edea25624951d6efc8c7f24d430dba0a8e007
Ben Avellone <benjamin.avellone@gmail.com>
2017-12-17 15:19:38 -0600

    Updated for React 16

--- src/components/graphiql.js
+++ src/components/graphiql.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import GraphiQL from 'graphiql';
 import { graphql } from 'graphql';
 
--- src/components/query.js
+++ src/components/query.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { isString, uniqueId, throttle } from 'lodash';
 import { graphql } from 'graphql';
 import { quickGraphql, parse } from '../quick-graphql';
